402e63953fe31fc56d357c9bf09c3aa992069501
dawei <dawei@163.com>
2024-11-19 09:18:33 +0800

    feat: 前端签名

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,6 +31,11 @@ const router = createRouter({
             component: () => import('../views/LoginView.vue')
         },
         {
+            path: '/file',
+            name: 'file',
+            component: () => import('../views/FileView.vue')
+        },
+        {
             path: '/home',
             name: 'home',
             component: HomeView
